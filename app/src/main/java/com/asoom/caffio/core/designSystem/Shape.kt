a3ba42a10ca0dfa860db29c8d8d8583277b676b2
AsmaaKhaledGomaa <asmaa.kh.gomaa@gmail.com>
2025-07-06 23:16:34 +0300

    feat: Add string resources and update shapes

--- app/src/main/java/com/asoom/caffio/core/designSystem/Shape.kt
+++ app/src/main/java/com/asoom/caffio/core/designSystem/Shape.kt
@@ -4,8 +4,9 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Shapes
 import androidx.compose.ui.unit.dp
 
-val CaffioShapes = Shapes(
+val CaffioShape = Shapes(
     small = RoundedCornerShape(8.dp),
     medium = RoundedCornerShape(16.dp),
-    large = RoundedCornerShape(24.dp)
+    large = RoundedCornerShape(24.dp),
+    extraLarge = RoundedCornerShape(100.dp)
 )
