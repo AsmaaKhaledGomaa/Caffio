fa808305fe777bcbd17ad11bbd07c7c7202aa410
AsmaaKhaledGomaa <asmaa.kh.gomaa@gmail.com>
2025-07-09 10:35:53 +0300

    feat: Add Accompanist, Koin, and Serialization dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
+    alias(libs.plugins.kotlin.serialization)
 }
 
 android {
@@ -52,4 +53,10 @@ dependencies {
     androidTestImplementation(libs.bundles.compose.test)
 
     debugImplementation(libs.bundles.compose.debug)
+
+    implementation(libs.bundles.pager)
+    implementation(libs.bundles.serialization)
+    implementation(libs.bundles.koin)
+
+    implementation(libs.bundles.accompanist)
 }
